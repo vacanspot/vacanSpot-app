1f54f1ebd2ca6c25464593c27938662abfa41fee
HaehunLee <allman956@naver.com>
2023-09-02 14:18:01 +0900

    Feat: 폰트 세팅 및 safeAreaView 대응

--- App.tsx
+++ App.tsx
@@ -1,8 +1,23 @@
-import {Text} from '@/components/atoms';
 import React from 'react';
+import {Text} from './src/components/atoms';
+import {
+  getStatusBarHeight,
+  getBottomSpace,
+} from 'react-native-iphone-screen-helper';
+import {styled} from 'styled-components/native';
+import {Platform} from 'react-native';
 
 function App(): JSX.Element {
-  return <Text>3413</Text>;
+  return (
+    <RootView>
+      <Text>vacanSpot</Text>
+    </RootView>
+  );
 }
 
 export default App;
+
+const RootView = styled.View`
+  padding-top: ${Platform.OS === 'android' ? 0 : getStatusBarHeight()}px;
+  padding-bottom: ${getBottomSpace()}px;
+`;
--- src/components/atoms/Text.tsx
+++ src/components/atoms/Text.tsx
@@ -1,4 +1,7 @@
 import {styled} from 'styled-components/native';
 
-const Text = styled.Text``;
+const Text = styled.Text`
+  font-size: 50px;
+  font-family: 'neurimboGothicRegular';
+`;
 export default Text;
