f87bc29380b61775b8f678659febfc46b5978226
imzeze <jihyekim019@gmail.com>
2023-11-12 22:55:21 +0900

    ing: splash 변경중

---
+++ ios/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  vacanSpot
+//
+//  Created by Jihye Kim on 2023/11/12.
+//
+
+import UIKit
+import Lottie
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
+
+ 
