e8c5d42d33d815a8ad52c513df645b27160ca8d4
imzeze <67260437+imzeze@users.noreply.github.com>
2023-11-14 17:27:13 +0900

    feat: asset 모듈화

---
+++ assets/index.js
@@ -0,0 +1,9 @@
+const assets = {
+  location: require('./icon-location.png'),
+  refresh: require('./icon-refresh.png'),
+  off: require('./icon-off.png'),
+  splash: require('./splash.json'),
+  logo: require('./vacanspot_logo.png'),
+};
+
+export default assets;
