531a698f8308c27f4048fb82c2d35644597b351d
imzeze <jihyekim019@gmail.com>
2023-11-16 17:51:22 +0900

    feat: 줌 기능 추가

--- src/components/organisms/CameraScreen.tsx
+++ src/components/organisms/CameraScreen.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {useEffect, useState} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {Dimensions, Platform, StyleSheet, View} from 'react-native';
 import {
   Camera,
   useCameraDevice,
+  useCameraFormat,
   useCameraPermission,
 } from 'react-native-vision-camera';
 import {COLORS} from '@/constants/colors';
@@ -12,18 +13,94 @@ import Assets from 'assets';
 import {Icon} from '@/components/atom';
 import {useRecoilState} from 'recoil';
 import {cameraFlashState} from '@/recoil/atom/camera';
+import Reanimated, {
+  Extrapolate,
+  interpolate,
+  useAnimatedGestureHandler,
+  useAnimatedProps,
+  useSharedValue,
+} from 'react-native-reanimated';
+import {
+  GestureHandlerRootView,
+  PinchGestureHandler,
+  PinchGestureHandlerGestureEvent,
+  TapGestureHandler,
+} from 'react-native-gesture-handler';
+import {useIsFocused} from '@react-navigation/core';
+import {useIsForeground} from '@/hook/useIsForeground';
+import {getBottomSpace} from 'react-native-iphone-screen-helper';
 
 interface CameraScreenProps {
   camera: React.RefObject<Camera>;
 }
 
+const ReanimatedCamera = Reanimated.createAnimatedComponent(Camera);
+Reanimated.addWhitelistedNativeProps({
+  zoom: true,
+});
+
 const CameraScreen = ({camera}: CameraScreenProps) => {
   const [deviceType, setDeviceType] = useState<'back' | 'front'>('back');
   const [showReqGrantModal, setShowReqGrantModal] = useState(false);
+  const [isOnFlash, setIsOnFlash] = useRecoilState(cameraFlashState);
+
+  const isFocussed = useIsFocused();
+  const isForeground = useIsForeground();
+  const isActive = isFocussed && isForeground;
+  const {hasPermission, requestPermission} = useCameraPermission();
 
   const device = useCameraDevice(deviceType);
-  const {hasPermission, requestPermission} = useCameraPermission();
-  const [isOnFlash, setIsOnFlash] = useRecoilState(cameraFlashState);
+  const zoom = useSharedValue(0);
+  const minZoom = device?.minZoom ?? 1;
+  const maxZoom = Math.min(device?.maxZoom ?? 1, 5);
+  const neutralZoom = device?.neutralZoom ?? 1;
+  const SCREEN_HEIGHT = Platform.select<number>({
+    android: Dimensions.get('screen').height - getBottomSpace(),
+    ios: Dimensions.get('window').height,
+  }) as number;
+
+  const screenAspectRatio = SCREEN_HEIGHT / Dimensions.get('window').width;
+
+  const format = useCameraFormat(device, [
+    {fps: 60},
+    {photoAspectRatio: screenAspectRatio},
+    {photoResolution: 'max'},
+  ]);
+
+  const cameraAnimatedProps = useAnimatedProps(() => {
+    const z = Math.max(Math.min(zoom.value, maxZoom), minZoom);
+    return {
+      zoom: z,
+    };
+  }, [maxZoom, minZoom, zoom]);
+
+  const onPinchGesture = useAnimatedGestureHandler<
+    PinchGestureHandlerGestureEvent,
+    {startZoom?: number}
+  >({
+    onStart: (_, context) => {
+      context.startZoom = zoom.value;
+    },
+    onActive: (event, context) => {
+      const startZoom = context.startZoom ?? 0;
+      const scale = interpolate(
+        event.scale,
+        [1 - 1 / 3, 1, 3],
+        [-1, 0, 1],
+        Extrapolate.CLAMP,
+      );
+      zoom.value = interpolate(
+        scale,
+        [-1, 0, 1],
+        [minZoom, startZoom, maxZoom],
+        Extrapolate.CLAMP,
+      );
+    },
+  });
+
+  useEffect(() => {
+    zoom.value = neutralZoom;
+  }, [neutralZoom, zoom]);
 
   useEffect(() => {
     if (!hasPermission) {
@@ -55,13 +132,24 @@ const CameraScreen = ({camera}: CameraScreenProps) => {
 
   return (
     <>
-      <Camera
-        style={styles.Camera}
-        device={device}
-        isActive={true}
-        ref={camera}
-        photo
-      />
+      <GestureHandlerRootView style={styles.Camera}>
+        <PinchGestureHandler onGestureEvent={onPinchGesture} enabled={isActive}>
+          <Reanimated.View style={StyleSheet.absoluteFill}>
+            <TapGestureHandler numberOfTaps={2}>
+              <ReanimatedCamera
+                ref={camera}
+                style={styles.Camera}
+                device={device}
+                format={format}
+                isActive={isActive}
+                enableZoomGesture={false}
+                animatedProps={cameraAnimatedProps}
+                photo
+              />
+            </TapGestureHandler>
+          </Reanimated.View>
+        </PinchGestureHandler>
+      </GestureHandlerRootView>
       <View style={styles.Controller}>
         <Icon
           type="Transparent"
