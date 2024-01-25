66d648aa44dd82958494a55d4956a157cc6962e2
imzeze <jihyekim019@gmail.com>
2024-01-25 23:23:30 +0900

    feat: code push 적용

--- index.js
+++ index.js
@@ -3,7 +3,20 @@
  */
 
 import {AppRegistry} from 'react-native';
+import codePush from 'react-native-code-push';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () =>
+  codePush({
+    updateDialog: {
+      title: '알림',
+      optionalUpdateMessage: '새로운 버전이 출시되었습니다.\n설치하시겠습니까?',
+      optionalIgnoreButtonLabel: '건너뛰기',
+      optionalInstallButtonLabel: '설치',
+    },
+    checkFrequency: codePush.CheckFrequency.ON_APP_START,
+    mandatoryInstallMode: codePush.InstallMode.ON_NEXT_RESTART,
+    installMode: codePush.InstallMode.IMMEDIATE,
+  })(App),
+);
--- ios/vacanSpot/AppDelegate.mm
+++ ios/vacanSpot/AppDelegate.mm
@@ -2,6 +2,7 @@
 #import <AppCenterReactNative.h>
 #import <AppCenterReactNativeAnalytics.h>
 #import <AppCenterReactNativeCrashes.h>
+#import <CodePush/CodePush.h>
 
 #import <React/RCTBundleURLProvider.h>
 
@@ -26,7 +27,7 @@
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  return [CodePush bundleURL];
 #endif
 }
 
